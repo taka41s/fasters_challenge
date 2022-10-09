eb7886f414aa7f8ba64116386a459c072f22766c
Taka41s <gutonunes99@gmail.com>
2022-10-09 11:30:56 -0300

    solving n+1 queries

--- app/controllers/parsers_controller.rb
+++ app/controllers/parsers_controller.rb
@@ -1,21 +1,14 @@
 class ParsersController < ApplicationController
   def show
-    @parser = HistoricalPrice.select(:month, :year, :open_price, :highest_price, :lowest_price, :volume, :close_price, :id).
-      where(year: params[:year], ticker: params[:ticker])
+    @parser = HistoricalPrice.select(:month, :year, :open_price, :highest_price, :lowest_price, :volume, :close_price, :id).where(year: params[:year], ticker: params[:ticker])
 
     if @parser.present? == false
       valid_tickers =  ["PETR4","VALE3","MGLU3"]
 
       if params[:ticker].in?(valid_tickers) && params[:year].present?
         @parser = Parser.new(archive_path: "#{params[:ticker]}.SA.csv", year: params[:year]).make
-
-        @parser.each do |parsed|
-          next unless parsed[1].present?
-          month = parsed[0].to_s
-          data = parsed[1].map{|x| x.to_h}.first.merge(ticker: params[:ticker])
-          @historical_price = HistoricalPrice.new(data)
-          @historical_price.save
-        end
+        data = @parser.map{|x| x[1][0].merge(ticker: params[:ticker])}
+        historical_price = HistoricalPrice.insert_all!(data)
 
         render json: {result: @parser.to_a.map{|x| x[1][0]}}, status: 200
       else
